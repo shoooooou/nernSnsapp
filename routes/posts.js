a1ec59713435ed4cb6f927749be47c84125bd959
s.nakamura <shotenba@gmail.com>
2023-07-23 16:23:38 +0900

    DB設定、APIの実装完了

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,103 @@
 const router = require("express").Router();
-router.get("/", (req, res) => {
-  res.send("posts router");
+const Post = require("../models/Post");
+const User = require("../models/User");
+//投稿を作成する
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    return res.status(200).json(savedPost);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//投稿を更新する
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId !== req.body.userId)
+      return res.status(403).json("他の人の投稿なので編集できません。");
+    await post.updateOne({
+      $set: req.body,
+    });
+    res.status(200).json("投稿の更新が完了しました。");
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//投稿を削除する
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletePost = await Post.findById(req.params.id);
+    if (deletePost.userId !== req.body.userId)
+      return res.status(403).json("他の人の投稿は削除できません。");
+    await deletePost.deleteOne();
+    res.status(200).json("投稿の削除に成功しました。");
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//投稿を取得する
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//特定の投稿にいいねを押す
+router.put("/:id/like", async (req, res) => {
+  try {
+    //投稿の情報を持ってくる
+    console.log(56);
+    console.log(req.params.id);
+    const post = await Post.findById(req.params.id);
+    console.log(60);
+    //既にいいねをしていなければユーザをpushする
+    if (!post.likes.includes(req.body.userId)) {
+      console.log(61);
+      await post.updateOne({
+        $push: {
+          likes: req.body.userId,
+        },
+      });
+      res.status(200).json("いいねをしました。");
+      //既にいいねをしていればユーザをpullする
+    } else {
+      console.log(69);
+      await post.updateOne({
+        $pull: {
+          likes: req.body.userId,
+        },
+      });
+      res.status(200).json("いいねを外しました。");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//タイムラインの投稿を取得する
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    console.log(currentUser);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    console.log("fP");
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    return res.status(200).json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,10 +4,27 @@ const PORT = 3000;
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postsRoute = require("./routes/posts");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//DB接続
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("DBと接続中・・");
+  })
+  .catch(() => {
+    console.error("DB接続エラー");
+  });
 
 //ミドルウェア
-app.use("/", userRoute);
-app.use("/auth", authRoute);
-app.use("/posts", postsRoute);
+app.use(express.json());
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
+
+app.get("/", (req, res) => {
+  res.send("hello express");
+});
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
